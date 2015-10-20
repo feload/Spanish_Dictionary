20e5a738be2cb9208ccd877f3ee3427ae4b90438
Elliot Cooper <github@elliotcooper.com>
2015-10-20 00:23:01 -0600

    Further work on the master list generating script

--- list_generator.py
+++ list_generator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python2
+# -*- coding: utf-8 -*-
 ### This program takes Spanish text files as input, strips non-words and punctuation and writes each word to a new line of the output file as long as the word does not already exist in the output file
 
-import string
+import nltk, codecs, string
 from os import listdir
 from os.path import isfile, join
 
@@ -9,38 +10,28 @@ from os.path import isfile, join
 source_words_dir = "source_words"
 source_files = []
 testing_file = "source_words/snippet.txt"
-word_lst = []
+dirty_word_lst = []
+clean_word_lst = []
 
+#Generate list of spanish word files
+for sourcefile in listdir(source_words_dir):
+    if isfile(join(source_words_dir, sourcefile)):
+        source_files.append(join(source_words_dir, sourcefile))
 
-def GetSourceFiles(path):
-    """ Function to grab a list of input word files from the source dirctory"""
-    for sourcefile in listdir(path):
-        if isfile(join(path, sourcefile)):
-            source_files.append(sourcefile)
-
-def MakeWordArray(word_file):
-    """Take a word file and crate a list containing every every word in the file"""
-    with open(word_file, 'rb') as f:
-        content = f.read()
-        table = string.maketrans(
-                string.punctuation,
-                ' '*len(string.punctuation))
-        content = content.translate(table).lower()
-        words = content.split()
-        print words
- 
- 
- #       content = content.translate(None, string.punctuation).lower()
- #       word_lst.append(content.split())
- #       return word_lst
+#Read files in utf-8 and append all words and punctuation to list
+with codecs.open(testing_file, 'r', "utf-8") as f:
+    content = f.read()
+    for word in nltk.word_tokenize(content):
+        dirty_word_lst.append(word.lower())
     
+for word in dirty_word_lst:
+    print word + "\n"
+
+   
+
+
+
     
-
-
-GetSourceFiles(source_words_dir)
-print source_files
-
-MakeWordArray(testing_file)
 
 
 
